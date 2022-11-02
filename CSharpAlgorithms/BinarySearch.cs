ec5292515dbce6c3eb8ee3ee94e793dfdc0adf58
shetty-niranjan <niranshettyofficial@gmail.com>
2022-11-02 23:36:36 +0530

    Test

---
+++ CSharpAlgorithms/BinarySearch.cs
@@ -0,0 +1,28 @@
+namespace Algorithms.CSharpAlgorithms
+{
+    public class BinarySearch
+    {
+        public int? Search(IList<int> list, int item)
+        {
+            var low = 0;
+            var high = list.Count() - 1;
+
+            while (low <= high)
+            {
+                var mid = (low + high) / 2;
+                var guess = list[mid];
+                if (guess == item) return mid;
+                if (guess > item)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Algorithms.CSharpAlgorithms;
+
+Console.WriteLine("Binary Search!");
+BinarySearch bs = new BinarySearch();
+var myList = new List<int> { 1, 3, 5, 7, 9 };
+Console.WriteLine(bs.Search(myList, 3)); // => 1
+Console.WriteLine(bs.Search(myList, 9)); // => 4
+Console.WriteLine(bs.Search(myList, 10)); //null gets printed as an empty string
+
